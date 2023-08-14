cf89343fe387e54f48f3b8977ae4651fdd3740e5
Oleg Chumin <tschumin.oleg@gmail.com>
2023-08-14 15:14:40 +0300

    CRUD code

--- src/main/java/OOP/OOP.java
+++ src/main/java/OOP/OOP.java
@@ -5,7 +5,7 @@ public class OOP {
         System.out.println("4 основных принципа ООП - Объектно-ориентированного программирования");
         System.out.println("Наследование Class_1 extends Class_2 implements Interface_1, Interface_2, ...");
         System.out.println("Полиморфизм - class Animal, class Dog, class Main{psvm{ Animal = new Dog() } }");
-        System.out.println("");
+        System.out.println("Инкапсуляция");
         System.out.println("");
     }
 }
